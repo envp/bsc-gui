c3dc5fc2281430a7a80070fd2248d733c1badded
vaibhav-y <vaibhav-y>
2014-03-17 11:28:52 +0100

    Refactor complete 	* Added progress bar (TODO Fix not working / unbound)

---
+++ bsc-gui/ctlBtnGrpConfirm.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ctlBtnGrpConfirm
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(0, 0)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(100, 23)
+        Me.btnOK.TabIndex = 23
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(106, 0)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(100, 23)
+        Me.btnCancel.TabIndex = 22
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'ctlBtnGrpConfirm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Name = "ctlBtnGrpConfirm"
+        Me.Size = New System.Drawing.Size(206, 23)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnOK As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+
+End Class
---
+++ bsc-gui/ctlBtnGrpConfirm.vb
@@ -0,0 +1,22 @@
+﻿Public Class ctlBtnGrpConfirm
+    Event ActionCancel()
+    Event ActionOK()
+
+    ' Proxy Init
+    Private Sub ctlBtnGrpConfirm_Initialize()
+
+    End Sub
+
+    Private Sub ctlBtnGrpConfirm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        ' Builder method pls
+        RaiseEvent ActionOK()
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        RaiseEvent ActionCancel()
+    End Sub
+End Class
